ffb82480b790dba96afbccdd26ff8a236a7974fd
MariaFernandaMelliana <202020432@mhs.udb.ac.id>
2022-08-07 10:54:00 +0700

    ganti server

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -11,7 +11,7 @@ const AddUser = () => {
   const saveUser = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/users", {
+      await axios.post("https://backend-nodejs-9.herokuapp.com/users", {
         name,
         email,
         gender,
